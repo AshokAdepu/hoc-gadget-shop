1f4b5f1ef22d67f17247fede0b1a68b375b68ed5
AshokAdepu <adepuashok9640@gmail.com>
2025-10-29 11:57:36 +0530

    new updates

---
+++ src/app/AppComponents/inventory/inventory.ts
@@ -0,0 +1,74 @@
+import { NgFor } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DialogBox } from '../../AppComponent/dialog-box/dialog-box';
+
+@Component({
+  selector: 'app-inventory',
+  imports: [FormsModule, NgFor],
+  templateUrl: './inventory.html',
+  styleUrl: './inventory.css'
+})
+export class Inventory implements OnInit {
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+  private httpClient = inject(HttpClient)
+  productIdToDelete: number = 0;
+  private modalservice = inject(NgbModal)
+
+  inventoryData = {
+    productId:"",
+    productName:"",
+    availableQty:0,
+    reorderPoint:0
+  }
+
+  inventoryDetails: any;
+
+  ngOnInit(){
+    this.getInventoryDetails();
+  }
+  getInventoryDetails(){
+    let apiUrl = "https://localhost:7027/api/Inventory";
+    this.httpClient.get(apiUrl).subscribe(data => {
+      this.inventoryDetails = data;
+      this.cdRef.detectChanges();
+    });
+  }
+
+  onSubmit(): void {
+  let apiUrl = "https://localhost:7027/api/Inventory";
+  let httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: "my-auth-token",
+      'Content-Type': "application/json"
+    })
+  }
+  this.httpClient.post(apiUrl,this.inventoryData,httpOptions).subscribe({
+    next: v => console.log(v),
+    error: e => console.log(e),
+    complete: () => {
+        alert("Form submitted succesfully" + JSON.stringify(this.inventoryData));
+        this.getInventoryDetails();
+    }
+  });
+  }
+
+  openConfirmDialog(productId: number){
+    this.productIdToDelete = productId;
+    this.modalservice.open(DialogBox).result.then( data => {
+      if(data.event == "confirm"){
+        this.deleteInventory();
+      }
+    })
+  }
+
+  deleteInventory(): void{
+    let apiUrl = "https://localhost:7027/api/Inventory?productId="+this.productIdToDelete;
+    this.httpClient.delete(apiUrl).subscribe(data => {
+      this.getInventoryDetails();
+    })
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Inventory } from './AppComponents/inventory/inventory';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path:'inventory', component:Inventory
+}];
