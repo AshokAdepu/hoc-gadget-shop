63cb5784a07ce956a3ea6cfc7dabdfba98515e43
AshokAdepu <adepuashok9640@gmail.com>
2025-10-29 14:24:39 +0530

    edit functinality added

--- src/app/AppComponents/inventory/inventory.ts
+++ src/app/AppComponents/inventory/inventory.ts
@@ -9,66 +9,92 @@ import { DialogBox } from '../../AppComponent/dialog-box/dialog-box';
   selector: 'app-inventory',
   imports: [FormsModule, NgFor],
   templateUrl: './inventory.html',
-  styleUrl: './inventory.css'
+  styleUrl: './inventory.css',
 })
 export class Inventory implements OnInit {
-
   constructor(private cdRef: ChangeDetectorRef) {}
-  private httpClient = inject(HttpClient)
+  private httpClient = inject(HttpClient);
   productIdToDelete: number = 0;
-  private modalservice = inject(NgbModal)
+  private modalservice = inject(NgbModal);
 
   inventoryData = {
-    productId:"",
-    productName:"",
-    availableQty:0,
-    reorderPoint:0
-  }
-
+    productId: '',
+    productName: '',
+    availableQty: 0,
+    reorderPoint: 0,
+  };
+  disableProductIdInput = false;
   inventoryDetails: any;
 
-  ngOnInit(){
+  ngOnInit() {
     this.getInventoryDetails();
   }
-  getInventoryDetails(){
-    let apiUrl = "https://localhost:7027/api/Inventory";
-    this.httpClient.get(apiUrl).subscribe(data => {
+  getInventoryDetails() {
+    let apiUrl = 'https://localhost:7027/api/Inventory';
+    this.httpClient.get(apiUrl).subscribe((data) => {
       this.inventoryDetails = data;
       this.cdRef.detectChanges();
     });
+    this.inventoryData = {
+      productId: '',
+      productName: '',
+      availableQty: 0,
+      reorderPoint: 0,
+    };
+    this.disableProductIdInput = false;
   }
 
   onSubmit(): void {
-  let apiUrl = "https://localhost:7027/api/Inventory";
-  let httpOptions = {
-    headers: new HttpHeaders({
-      Authorization: "my-auth-token",
-      'Content-Type': "application/json"
-    })
-  }
-  this.httpClient.post(apiUrl,this.inventoryData,httpOptions).subscribe({
-    next: v => console.log(v),
-    error: e => console.log(e),
-    complete: () => {
-        alert("Form submitted succesfully" + JSON.stringify(this.inventoryData));
-        this.getInventoryDetails();
+    let apiUrl = 'https://localhost:7027/api/Inventory';
+    let httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'my-auth-token',
+        'Content-Type': 'application/json',
+      }),
+    };
+    if (this.disableProductIdInput == true) {
+      this.httpClient.put(apiUrl, this.inventoryData, httpOptions).subscribe({
+        next: (v) => console.log(v),
+        error: (e) => console.log(e),
+        complete: () => {
+          alert('Form submitted succesfully' + JSON.stringify(this.inventoryData));
+          this.getInventoryDetails();
+        },
+      });
+    } else {
+      this.httpClient.post(apiUrl, this.inventoryData, httpOptions).subscribe({
+        next: (v) => console.log(v),
+        error: (e) => console.log(e),
+        complete: () => {
+          alert('Form submitted succesfully' + JSON.stringify(this.inventoryData));
+          this.getInventoryDetails();
+        },
+      });
     }
-  });
   }
 
-  openConfirmDialog(productId: number){
+  openConfirmDialog(productId: number) {
     this.productIdToDelete = productId;
-    this.modalservice.open(DialogBox).result.then( data => {
-      if(data.event == "confirm"){
+    this.modalservice.open(DialogBox).result.then((data) => {
+      if (data.event == 'confirm') {
         this.deleteInventory();
       }
-    })
+    });
   }
 
-  deleteInventory(): void{
-    let apiUrl = "https://localhost:7027/api/Inventory?productId="+this.productIdToDelete;
-    this.httpClient.delete(apiUrl).subscribe(data => {
+  deleteInventory(): void {
+    let apiUrl = 'https://localhost:7027/api/Inventory?productId=' + this.productIdToDelete;
+    this.httpClient.delete(apiUrl).subscribe((data) => {
       this.getInventoryDetails();
-    })
+    });
+  }
+
+  populateFormForEdit(inventory: any) {
+    this.inventoryData.productId = inventory.productId;
+    this.inventoryData.productName = inventory.productName;
+    this.inventoryData.availableQty = inventory.availableQty;
+    this.inventoryData.reorderPoint = inventory.reOrderPoint;
+
+    this.disableProductIdInput = true;
   }
 }
